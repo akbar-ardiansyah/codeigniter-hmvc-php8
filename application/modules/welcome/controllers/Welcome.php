f3a357a8a10e4f6ba45c6b2b8046554bd4356415
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2022-12-22 13:41:59 +0700

    rename controller modules

--- application/modules/welcome/controllers/Welcome.php
+++ application/modules/welcome/controllers/Welcome.php
@@ -3,10 +3,10 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 
-class Test extends CI_Controller
+class Welcome extends CI_Controller
 {
     public function index()
     {
-        echo 'helo';
+        $this->load->view('welcome_message');
     }
 }
