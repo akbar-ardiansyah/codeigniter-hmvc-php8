b3f59b53829c0e50e438e7b84fd889af75bf45f6
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2022-12-22 13:33:03 +0700

    instalation codeigniter php 8x

---
+++ application/modules/main/controllers/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+class Test extends CI_Controller
+{
+    public function index()
+    {
+        echo 'helo';
+    }
+}
