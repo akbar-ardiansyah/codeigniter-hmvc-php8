8b2b447596771895fe7264c7d38682d12c527424
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2022-12-26 11:40:39 +0700

    define hooks to extend .env

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/userguide3/general/hooks.html
 |
 */
+
+# Load phpdotenv
+$hook['pre_system'] = function () {
+    $dotenv = Dotenv\Dotenv::create(APPPATH);
+    $dotenv->load();
+};
